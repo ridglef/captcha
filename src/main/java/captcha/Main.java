fc15257eb47796b18490e030bac19fae01694849
ridglef <126202739+ridglef@users.noreply.github.com>
2024-10-12 09:22:30 -0400

    significant improvement

--- src/main/java/captcha/Main.java
+++ src/main/java/captcha/Main.java
@@ -8,10 +8,8 @@ import com.google.gson.JsonParser;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.awt.image.RescaleOp;
+import java.io.*;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -22,6 +20,16 @@ public class Main {
     private static final Gson gson = new Gson();
     private static final Random random = ThreadLocalRandom.current();
     private static final int range = 20;
+    private static final BufferedImage sillyCat;
+
+    static {
+        try {
+            InputStream is = Main.class.getResourceAsStream("/sillyCat.jpg");
+            sillyCat = ImageIO.read(is);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static String getNearbyMcdonaldsLocationUrl() {
         String jsonResponse;
@@ -53,20 +61,28 @@ public class Main {
 
         g2d.setColor(Color.WHITE);
         g2d.fillRect(0, 0, width, height);
-        for (int i = 0; i < width; i++) {
-            g2d.setColor(random.nextBoolean() ? Color.WHITE : Color.LIGHT_GRAY);
-            g2d.fillRect(random.nextInt(width), random.nextInt(height), random.nextInt(width), random.nextInt(height));
+        for (int i = 0; i < height; i++) {
+            for (int b = 0; b < width; b++) {
+                g2d.setColor(random.nextBoolean() ? Color.WHITE : Color.LIGHT_GRAY.darker());
+                g2d.fillRect(i,b,1,1);
+            }
         }
 
-        g2d.setFont(new Font("Arial", Font.BOLD, 34));
+        for (int i = 0; i < 16; i++) {
+            RescaleOp op = new RescaleOp(new float[] {random.nextFloat(), random.nextFloat(), random.nextFloat()}, new float[3], null);
+            BufferedImage tintedCat = op.filter(sillyCat, null);
+            g2d.drawImage(tintedCat, null, random.nextInt(width - sillyCat.getWidth()), random.nextInt(height - sillyCat.getHeight()));
+        }
+
+        g2d.setFont(new Font("Arial", Font.BOLD, 44));
         g2d.rotate(Math.toRadians(random.nextBoolean() ? random.nextInt(21) : -random.nextInt(21)));
-        g2d.setColor(Color.BLACK);
+        g2d.setColor(Color.RED.darker());
 
         g2d.drawString(str, 350, 350);
 
         g2d.setColor(Color.LIGHT_GRAY);
 
-        for (int i = 0; i < 800; i++) {
+        for (int i = 0; i < 400; i++) {
             int x1 = random.nextInt(width);
             int y1 = random.nextInt(height);
             int x2 = random.nextInt(width);
@@ -79,7 +95,7 @@ public class Main {
         return bufferedImage;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String jsonString;
 
         try {
@@ -108,7 +124,11 @@ public class Main {
 
             BufferedImage captcha = buildCaptcha(mcAddress + upperCaseRandom + lowerCaseRandom);
 
-            ImageIO.write(captcha, "png", new File("captcha.png"));
+            try {
+                ImageIO.write(captcha, "png", new File("captcha.png"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
 
             System.out.println("Captcha Created");
             System.out.println("Instructions: Find the Opening Hour of the listed McDonalds and append the 2 last characters to the end. (CASE SENSITIVE)");
